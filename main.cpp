cc2acac33a2f0a308cfa3c0ee0b76b10c682f4fe
MateuszNowak123 <mateusz726@onet.eu>
2020-02-29 18:39:03 +0100

    add CmakeLists.txt and main.cpp part2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Worald!" << std::endl;
+    std::cout << "Hello, Worlasdsa!" << std::endl;
+    std::cout << "Hello, Worlsaasaasd!" << std::endl;
+    std::cout << "Hello, Worldasa!" << std::endl;
     return 0;
 }
