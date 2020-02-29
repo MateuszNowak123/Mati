d7ec4e56390ba5afaff847c59027325055243dc2
MateuszNowak123 <mateusz726@onet.eu>
2020-02-29 18:32:07 +0100

    add CmakeLists.txt and main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,8 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, !" << std::endl;
+    std::cout << "cotam jak tam, !" << std::endl;
+    std::cout << "powoli !" << std::endl;
     return 0;
 }
